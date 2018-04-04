d59e576e9c43db598fdfd235fbfb1a68e9b9ab28
David Seidenberg <davidseidenberg@gmail.com>
2018-04-04 12:20:15 -0700

    Initialize App component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const title = 'CrytoCharter';
+import App from './App.js';
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <App />,
   document.getElementById('app')
 );
